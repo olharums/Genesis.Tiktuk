12fe14398e9069bb27adbdaaaf4cec06378ecbd4
olharums <54138100+olharums@users.noreply.github.com>
2021-12-23 12:33:08 +0200

    Delete src directory

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.css";
-import WrappedApp from "./WrappedApp";
-
-ReactDOM.render(<WrappedApp />, document.getElementById("root"));
--- src/utils/ShortenNumber.jsx
+++
@@ -1,9 +0,0 @@
-const ShortenNumber = (number) => {
-  if (number / 10000 < 1) {
-    return number;
-  }
-  const value = Math.round(number / 100) / 10;
-  return `${value}K`;
-};
-
-export default ShortenNumber;
